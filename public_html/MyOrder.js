a9793c8c72b0a5f77c94915ea7ca9769e37f8dda
Shaun <Shaun@192.168.2.5>
2014-07-30 15:40:11 +0800

    20140730_1424

--- public_html/MyOrder.js
+++ public_html/MyOrder.js
@@ -1,37 +1,16 @@
 var myOrder = angular.module('myOrder', []);
 var MyOrderController = myOrder.controller('MyOrderController', function() {
+    this.remember = function(obj, propertyName, value) {
+        obj[propertyName] = value;
+        localStorage.setItem(propertyName, value);
+    };
 
-    function rememberBoolean(propertyName, localStorageKey, defaultValue) {
-        if (localStorage.getItem(localStorageKey) === null) {
-            localStorage.setItem(localStorageKey, defaultValue);
-        }
-        Object.defineProperty(this, propertyName, {
-            get: function() {
-                return localStorage.getItem(localStorageKey) === 'true';
-            },
-            set: function(value) {
-                localStorage.setItem(localStorageKey, value ? 'true' : 'false');
-            }
-        });
-    }
-    function rememberString(propertyName, localStorageKey, defaultValue) {
-        if (localStorage.getItem(localStorageKey) === null) {
-            localStorage.setItem(localStorageKey, defaultValue);
-        }
-        Object.defineProperty(this, propertyName, {
-            get: function() {
-                return localStorage.getItem(localStorageKey);
-            },
-            set: function(value) {
-                if (value !== undefined) {
-                    localStorage.setItem(localStorageKey, value === undefined ? '' : value);
-                }
-            }
-        });
-    }
-    this.instructions = new rememberBoolean('visible', 'instructionsVisible', true);
-    this.favourites = new rememberBoolean('visible', 'favouritesVisible', true);
-    this.stomaApplianceScheme = new rememberBoolean('visible', 'stomaApplianceSchemeVisible', true);
-    this.myOrderName = new rememberString('text', 'myOrderName', '');
-    this.test = '12345';
+    localStorage.removeItem('instructionsVisible');
+    localStorage.removeItem('favouritesVisible');
+    localStorage.removeItem('stomaApplianceSchemeVisible');
+    this.instructionsVisible = localStorage.getItem('instructionsVisible') === null ? true : localStorage.getItem('instructionsVisible') === 'true';
+    this.favouritesVisible = localStorage.getItem('favouritesVisible') === null ? true : localStorage.getItem('favouritesVisible') === 'true';
+    this.stomaApplianceSchemeVisible = localStorage.getItem('stomaApplianceSchemeVisible') === null ? true : localStorage.getItem('stomaApplianceSchemeVisible') === 'true';
+
+//    this.myOrderName = new rememberString('text', 'myOrderName', '');
 });
